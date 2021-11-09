4c08f9d6352735b3025c64b8ff79b7c6b53b1dc1
Offem-Peter <nescooffem@gmail.com>
2021-11-09 14:56:47 +0100

    (chore) cleanup

--- src/schema/metrics.schema.ts
+++ src/schema/metrics.schema.ts
@@ -1,11 +1,11 @@
 import { object, string } from 'yup';
 
 const createMetricsSchema = () => {
-	return object({
-		body: object({
-			name: string().required(`property 'name' is required`),
-		}),
-	});
+  return object({
+    body: object({
+      name: string().required(`body 'name' is required`),
+    }),
+  });
 };
 
 export { createMetricsSchema };
